6782bacef56d6d99a438a3b9226fdd165346e6bb
hansjensen <grenademisfits@gmail.com>
2022-12-28 00:16:55 -0800

    Complete functions for quality buttons and reset.

--- script.js
+++ script.js
@@ -1,17 +1,66 @@
 let container = document.querySelector('.sketch');
 let drawsquare = document.createElement('div');
+document.getElementById("reset").onclick = reset;
+document.getElementById("low").onclick = low;
+document.getElementById("med").onclick = med;
+document.getElementById("hi").onclick = hi;
+
+low();
 
 
-for (let i = 0; i < 128; i++) {
-    drawsquare = document.createElement('div');
-    drawsquare.classList.add('draw');
-    container.appendChild(drawsquare);
+
+
+function reset() {
+    const ghost = document.querySelectorAll('div.draw');
+    ghost.forEach((div) => {
+        div.classList.remove('drawhover');
+    });
 }
 
-const ghost = document.querySelectorAll('div.draw');
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
-ghost.forEach((div) => {
-    div.addEventListener('mouseover', (event) => {
-        div.classList.add('drawhover');
-      });
-});
+function hover () {
+    const ghost = document.querySelectorAll('div.draw');
+    ghost.forEach((div) => {
+        div.addEventListener('mouseover', (event) => {
+            div.classList.add('drawhover');
+          });
+    });
+    
+}
+
+function low() {
+    removeAllChildNodes(container);
+    for (let i = 0; i < 512; i++) {
+        drawsquare = document.createElement('div');
+        drawsquare.classList.add('draw', 'drawlow');
+        container.appendChild(drawsquare);
+    }
+    hover();
+}
+
+function med() {
+    removeAllChildNodes(container);
+    for (let i = 0; i < 2048; i++) {
+        drawsquare = document.createElement('div');
+        drawsquare.classList.add('draw', 'drawmed');
+        container.appendChild(drawsquare);
+    }
+    hover();
+}
+
+function hi() {
+    removeAllChildNodes(container);
+    for (let i = 0; i < 8192; i++) {
+        drawsquare = document.createElement('div');
+        drawsquare.classList.add('draw', 'drawhi');
+        container.appendChild(drawsquare);
+    }
+    hover();
+}
+
+
